69d5c66a7eeee28e15aa1da4cbd0ce27271f833d
mosama88 <mosama88@hotmail>
2025-03-26 02:12:38 +0200

    Done Good3

--- app/Http/Controllers/Dashboard/LeaveBalanceController.php
+++ app/Http/Controllers/Dashboard/LeaveBalanceController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use App\Models\LeaveBalance;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\LeaveBalanceRequest;
 
 class LeaveBalanceController extends Controller
 {
@@ -12,7 +14,8 @@ class LeaveBalanceController extends Controller
      */
     public function index()
     {
-        //
+        $data = LeaveBalance::orderByDesc('id')->paginate(10);
+        return view('dashboard.leaveBalances.index', compact('data'));
     }
 
     /**
@@ -20,46 +23,68 @@ class LeaveBalanceController extends Controller
      */
     public function create()
     {
-        //
+        // $other['governorates'] = Governorate::get();
+        return view('dashboard.leaveBalances.create', compact('other'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(LeaveBalanceRequest $request)
     {
-        //
+        $leaveBalancees = $request->validated();
+        $data = array_merge($leaveBalancees, [
+            'created_by' => auth()->guard('admin')->user()->id,
+        ]);
+        LeaveBalance::create($data);
+        session()->flash('success', 'تم أضافة الفرع بنجاح');
+
+        return redirect()->route('dashboard.leaveBalances.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(LeaveBalance $leaveBalance)
     {
-        //
+        // $other['governorates'] = Governorate::get();
+
+        return view('dashboard.leaveBalances.show', compact('leaveBalance', 'other'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(LeaveBalance $leaveBalance)
     {
-        //
+        // $other['governorates'] = Governorate::get();
+
+        return view('dashboard.leaveBalances.edit', compact('leaveBalance', 'other'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(LeaveBalanceRequest $request, LeaveBalance $leaveBalance)
     {
-        //
+        $leaveBalance->fill($request->validated());
+        $leaveBalance->updated_by = auth()->guard('admin')->user()->id;
+
+        $leaveBalance->update();
+        session()->flash('success', 'تم تعديل الفرع بنجاح');
+
+        return redirect()->route('dashboard.leaveBalances.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(LeaveBalance $leaveBalance)
     {
-        //
+        $leaveBalance->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'تم حذف الفرع بنجاح!'
+        ]);
     }
 }
--- app/Http/Requests/Dashboard/LeaveBalanceRequest.php
+++ app/Http/Requests/Dashboard/LeaveBalanceRequest.php
@@ -11,7 +11,7 @@ class LeaveBalanceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
