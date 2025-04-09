0302cf473d2ed3c6cda1c951bcbfa136396130f7
mosama88 <mosama88@hotmail>
2025-04-09 19:39:00 +0200

    Done good

--- resources/views/dashboard/partials/action.blade.php
+++ resources/views/dashboard/partials/action.blade.php
@@ -1,3 +1,136 @@
+<div class="flex align-items-center list-user-action">
+    <a class="text-success pe-2" data-bs-toggle="tooltip" data-placement="top" title="" data-original-title="Add"
+        href="{{ route('dashboard.' . $name . '.show', $name_id) }}">
+        <span class="btn-inner">
+
+            <svg class="icon-20" width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M11.9849 15.3462C8.11731 15.3462 4.81445 15.931 4.81445 18.2729C4.81445 20.6148 8.09636 21.2205 11.9849 21.2205C15.8525 21.2205 19.1545 20.6348 19.1545 18.2938C19.1545 15.9529 15.8735 15.3462 11.9849 15.3462Z"
+                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M11.9849 12.0059C14.523 12.0059 16.5801 9.94779 16.5801 7.40969C16.5801 4.8716 14.523 2.81445 11.9849 2.81445C9.44679 2.81445 7.3887 4.8716 7.3887 7.40969C7.38013 9.93922 9.42394 11.9973 11.9525 12.0059H11.9849Z"
+                    stroke="currentColor" stroke-width="1.42857" stroke-linecap="round" stroke-linejoin="round">
+                </path>
+            </svg>
+
+        </span>
+    </a>
+    <a class="text-warning pe-2" data-bs-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"
+        href="{{ route('dashboard.' . $name . '.edit', $name_id) }}">
+        <span class="btn-inner">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.31055 14.3321H14.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                    stroke-linejoin="round"></path>
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M8.58501 1.84609C9.16674 1.15084 10.2125 1.04889 10.9222 1.6188C10.9614 1.64972 12.2221 2.62909 12.2221 2.62909C13.0017 3.10039 13.244 4.10233 12.762 4.86694C12.7365 4.90789 5.60896 13.8234 5.60896 13.8234C5.37183 14.1192 5.01187 14.2938 4.62718 14.298L1.89765 14.3323L1.28265 11.7292C1.1965 11.3632 1.28265 10.9788 1.51978 10.683L8.58501 1.84609Z"
+                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M7.26562 3.50073L11.3548 6.64108" stroke="currentColor" stroke-width="1.5"
+                    stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+        </span>
+    </a>
+
+    <form id="delete-form-{{ $name_id }}" action="{{ route('dashboard.' . $name . '.destroy', $name_id) }}"
+        method="POST" style="display: none;">
+        @csrf
+        @method('DELETE')
+    </form>
+    @can('حذف الموظف')
+        <a class="text-danger delete-btn" id="delete_one" data-id="{{ $name_id }}" data-bs-toggle="tooltip"
+            data-placement="top" title="" data-original-title="Delete" href="#">
+            <span class="btn-inner">
+                <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M12.4938 6.10107C12.4938 6.10107 12.0866 11.1523 11.8503 13.2801C11.7378 14.2963 11.1101 14.8918 10.0818 14.9106C8.12509 14.9458 6.16609 14.9481 4.21009 14.9068C3.22084 14.8866 2.60359 14.2836 2.49334 13.2853C2.25559 11.1388 1.85059 6.10107 1.85059 6.10107"
+                        stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M13.5312 3.67969H0.812744" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round"></path>
+                    <path
+                        d="M11.0804 3.67974C10.4917 3.67974 9.98468 3.26349 9.86918 2.68674L9.68693 1.77474C9.57443 1.35399 9.19343 1.06299 8.75918 1.06299H5.58443C5.15018 1.06299 4.76918 1.35399 4.65668 1.77474L4.47443 2.68674C4.35893 3.26349 3.85193 3.67974 3.26318 3.67974"
+                        stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </span>
+        </a>
+    @endcan
+
+</div>
+
+
+@push('js')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Attach event listener to delete buttons
+            document.querySelectorAll('.delete-btn').forEach(function(button) {
+                button.addEventListener('click', function(event) {
+                    event.preventDefault(); // Prevent default behavior
+
+                    // Retrieve the form ID from the button's data attribute
+                    const nameId = this.getAttribute('data-id');
+                    const form = document.getElementById(`delete-form-${nameId}`);
+
+                    // Display SweetAlert confirmation dialog
+                    Swal.fire({
+                        title: "هل أنت متأكد ؟",
+                        text: "أنت على وشك حذف العناصر المحددة!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonText: "إحذف",
+                        cancelButtonText: "إلغاء",
+                        reverseButtons: true
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            // Perform AJAX request
+                            fetch(form.action, {
+                                    method: 'POST',
+                                    body: new FormData(form),
+                                    headers: {
+                                        'X-CSRF-TOKEN': "{{ csrf_token() }}" // Add CSRF token
+                                    }
+                                })
+                                .then(response => response.json())
+                                .then(data => {
+                                    if (data.success) {
+                                        Swal.fire({
+                                            title: "تم الحذف",
+                                            text: data
+                                                .message, // هذه الرسالة تأتي من الـ Controller
+                                            icon: 'success',
+                                            timer: 1500,
+                                            showConfirmButton: false
+                                        }).then(() => {
+                                            location
+                                                .reload(); // Reload the page
+                                        });
+                                    } else {
+                                        Swal.fire({
+                                            title: "خطأ!",
+                                            text: data.message ||
+                                                "حدث خطأ غير متوقع",
+                                            icon: 'error'
+                                        });
+                                    }
+                                })
+                                .catch(error => {
+                                    Swal.fire({
+                                        title: "خطأ!",
+                                        text: "حدث خطأ غير متوقع",
+                                        icon: 'error'
+                                    });
+                                });
+                        }
+                    });
+                });
+            });
+        });
+    </script>
+@endpush
+
+
+
+
+{{-- 
 <a class="btn btn-primary btn-sm" href="{{ route('dashboard.' . $name . '.show', $name_id) }}">
     <i class="fas fa-folder">
     </i>
@@ -88,3 +221,4 @@
         });
     </script>
 @endpush
+ --}}
