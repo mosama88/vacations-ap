b05624f986ae158220c6f45215fa828ea8a07415
mosama88 <mosama88@hotmail>
2025-04-20 21:49:26 +0200

    الأجازات

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -9,12 +9,6 @@
 @section('content')
 
 
-    {{-- @include('dashboard.layouts.breadcrumb', [
-        'pageTitle' => 'بروفايل الموظف',
-        'previousPage' => '',
-        'urlPreviousPage' => '',
-        'currentPage' => 'الصفحه الرئيسية',
-    ]) --}}
     @include('dashboard.layouts.message')
 
 
@@ -28,7 +22,7 @@
                         <div class="inner">
                             <h3>150</h3>
 
-                            <p>New Orders</p>
+                            <p>إجمالى الأجازات</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-bag"></i>
@@ -43,7 +37,8 @@
                         <div class="inner">
                             <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-                            <p>Bounce Rate</p>
+                            <p>إجمالى الأجازات المعلقه</p>
+
                         </div>
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
@@ -56,9 +51,9 @@
                     <!-- small box -->
                     <div class="small-box bg-warning">
                         <div class="inner">
-                            <h3>44</h3>
+                            <h3>70</h3>
 
-                            <p>User Registrations</p>
+                            <p>إجمالى الموظفين بالنيابه</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-person-add"></i>
