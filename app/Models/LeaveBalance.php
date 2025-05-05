f71281e60f662100a1bb5a28c591534aa5743226
mosama <mosama88@mail.com>
2025-05-05 13:59:57 +0300

    LeaveBalanceObserver

--- app/Models/LeaveBalance.php
+++ app/Models/LeaveBalance.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
-#[ObservedBy([LeaveBalanceObserver::class])]
 class LeaveBalance extends Model
 {
     use HasFactory;
